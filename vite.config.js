d40d5de7084c63ea0d0c3d6db80ffcb5756145e7
Dan Zaharia <zaharia.danny@gmail.com>
2025-09-17 18:40:56 -0400

    Change vite config to try to resolve react router issues

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: '/',
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets'
+  // }
 })
