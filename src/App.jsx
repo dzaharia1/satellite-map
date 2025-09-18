b2a1715f11ea4d6fdc959fee6d6244c0104ff2a1
Dan Zaharia <zaharia.danny@gmail.com>
2025-09-18 19:30:17 -0400

    Style the map for eink

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { renderToStaticMarkup } from "react-dom/server";
 import { convertDmsToDecimal } from "./coordinates.js";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-
+ 
 // E-ink styles
 const eInkStyles = `
 // .leaflet-tile-pane {
@@ -12,15 +13,40 @@ const eInkStyles = `
 // }
 `;
 
-// You may need to adjust the icon paths based on your project structure
-const defaultIcon = new L.Icon({
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-  shadowSize: [41, 41]
-});
+const SatelliteMarker = ({ satellite }) => {
+  const iconMarkup = renderToStaticMarkup(
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <circle cx="35" cy="35" r="35" fill="white" stroke="black" strokeWidth="2" />
+      <circle cx="35" cy="35" r="20" fill="black" />
+      <def>
+        <path id="text-path" d="M 10,35 a 25,25 0 1,1 50,0 a 25,25 0 1,1 -50,0" transform="rotate(180) scale(.7, .7)" />
+      </def>
+      <text fontFamily="monospace" fontSize="12" fill="black" fontWeight={700}>
+        <textPath href="#text-path" startOffset="50%" textAnchor="center">
+          {satellite.satname}
+        </textPath>
+      </text>
+    </svg>
+  );
+  
+  const customIcon = new L.DivIcon({
+    html: iconMarkup,
+    className: "dummy", // needed
+    iconSize: [35, 35]
+  });
+  
+  return (
+    <Marker position={[satellite.satlat, satellite.satlng]} icon={customIcon}>
+      <Popup>
+        <b>{satellite.satname}</b>
+        <br />
+        Launched {satellite.launchDate}
+        <br />
+        Altitude: {satellite.satalt} km
+      </Popup>
+    </Marker>
+  );
+};
 
 function App() {
   let { location } = useParams();
@@ -78,23 +104,11 @@ function App() {
         style={{ height: "100vh", width: "100%" }}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &amp; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
         />
         {satellites.map((satellite) => (
-          <Marker
-            key={satellite.satid}
-            position={[satellite.satlat, satellite.satlng]}
-            icon={defaultIcon}
-          >
-            <Popup>
-              <b>{satellite.satname}</b>
-              <br />
-              Launched {satellite.launchDate}
-              <br />
-              Altitude: {satellite.satalt} km
-            </Popup>
-          </Marker>
+          <SatelliteMarker key={satellite.satid} satellite={satellite} />
         ))}
       </MapContainer>
     </>
